8f7274fa47640c139e7f746719a178e9da18a1cb
Nahom <nahomseyfu3@gmail.com>
2023-12-19 04:12:35 -0800

    last from the scratch

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,13 +6,10 @@
  */
 int print_last_digit(int n)
 {
-	int last;
-	
-	last = n % 10;
-	if (last < 0)
-	{
+	int last = n % 10;
+
+	if (n < 0)
 		last = last * -1;
-	}
 	_putchar(last + '0');
 	return (last);
 }
